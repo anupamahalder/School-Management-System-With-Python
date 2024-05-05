cee6cef4bd6b8afd05ca7d66d68dca2d4fdb92e0
Anupama Halder <110773499+anupamahalder@users.noreply.github.com>
2024-05-05 17:15:42 +0530

    Added output of the main program

---
+++ output.py
@@ -0,0 +1,105 @@
+# Here is the output of the main file after compilation
+---------All Classrooms---------
+
+Eight
+Nine
+Ten
+
+---------All Students---------
+
+-----EIGHT Classroom Stduents
+Bluyi
+Sabana
+-----NINE Classroom Stduents
+Priya
+Afreen
+-----TEN Classroom Stduents
+Firoza
+
+
+-----EIGHT Classroom Subjects
+Bangla
+Physics
+Chemistry
+Mathematics
+-----NINE Classroom Subjects
+Bangla
+Biology
+Mathematics
+-----TEN Classroom Subjects
+Physics
+MCA
+English
+Biology
+
+
+
+-----------Stdudent Results-----------
+
+
+        =============================================================
+        Student Name: Bluyi | Class: Eight | Roll No: Eight-1
+        =============================================================
+
+        Subject         Marks   Grade
+        ----------------------------------
+        Bangla          82      A+
+        Physics         99      A+
+        Chemistry       54      B
+        Mathematics     100     A+
+        ----------------------------------
+        Final grade: A+
+
+
+        =============================================================
+        Student Name: Sabana | Class: Eight | Roll No: Eight-2
+        =============================================================
+
+        Subject         Marks   Grade
+        ----------------------------------
+        Bangla          81      A+
+        Physics         94      A+
+        Chemistry       85      A+
+        Mathematics     55      B
+        ----------------------------------
+        Final grade: A+
+
+
+        =============================================================
+        Student Name: Priya | Class: Nine | Roll No: Nine-1
+        =============================================================
+
+        Subject         Marks   Grade
+        ----------------------------------
+        Bangla          98      A+
+        Biology         70      A
+        Mathematics     57      B
+        ----------------------------------
+        Final grade: A
+
+
+        =============================================================
+        Student Name: Afreen | Class: Nine | Roll No: Nine-2
+        =============================================================
+
+        Subject         Marks   Grade
+        ----------------------------------
+        Bangla          58      B
+        Biology         53      B
+        Mathematics     80      A+
+        ----------------------------------
+        Final grade: A
+
+
+        =============================================================
+        Student Name: Firoza | Class: Ten | Roll No: Ten-1
+        =============================================================
+
+        Subject         Marks   Grade
+        ----------------------------------
+        Physics         70      A
+        MCA             98      A+
+        English         73      A
+        Biology         51      B
+        ----------------------------------
+        Final grade: A
